5918b62d3ada42437a882a33415873a1326279c4
RaiAnant <iit2017103@iiita.ac.in>
2018-06-21 15:00:02 +0530

    commit

--- DatabaseApp/app/src/main/java/com/example/anant/databaseapp/DrinkCategory.java
+++ DatabaseApp/app/src/main/java/com/example/anant/databaseapp/DrinkCategory.java
@@ -10,12 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class DrinkCategory extends AppCompatActivity {
 
@@ -26,18 +24,7 @@ public class DrinkCategory extends AppCompatActivity {
         Intent intent=getIntent();
         final int id=intent.getIntExtra("position",0);
         ListView listView=findViewById(R.id.drink_category);
-//        Products[] products;
-//        if(id==0) {
-//            products = Products.drinks;
-//        }
-//        else{
-//            products = Products.foods;
-//        }
-//        int len=products.length;
-//        String[] strings=new String[len];
-//        for(int i=0;i<len;i++){
-//            strings[i]=products[i].getName();
-//        }
+
 
         SQLiteOpenHelper starBucksHelper = new StarbucksDatabaseHelper(this);
         try{
@@ -45,8 +32,7 @@ public class DrinkCategory extends AppCompatActivity {
             String[] DATABASE_NAMES={"DRINK","FOOD"};
             long count = DatabaseUtils.queryNumEntries(db, DATABASE_NAMES[id]);
             String[] strings=new String[(int)count];
-            Cursor cursor=db.query(DATABASE_NAMES[id],new String[] {"NAME",
-                    "DESCRIPTION","IMAGE_RESOURCE_ID"},
+            Cursor cursor=db.query(DATABASE_NAMES[id],new String[] {"_id","NAME"},
                     null,
                     null,
                     null,
@@ -55,29 +41,21 @@ public class DrinkCategory extends AppCompatActivity {
                     null);
             if(cursor.moveToFirst()){
 
-                for(int i=0;i<strings.length;i++){
-                    strings[i]=cursor.getString(0);
-                    if(cursor.moveToNext()){
-                        continue;
+
+                SimpleCursorAdapter cursorAdapter=new SimpleCursorAdapter(this,R.layout.drinks,cursor,new String[]{"NAME"},new int[] {R.id.drink},0);
+                listView.setAdapter(cursorAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent=new Intent(DrinkCategory.this,DetailActivity.class);
+                        intent.putExtra("position",id);
+                        intent.putExtra("product",i);
+                        startActivity(intent);
                     }
-                    break;
-                }
+                });
             }
 
 
-            ArrayList<String> arrayList=new <String>ArrayList(Arrays.asList(strings));
-
-            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.drinks,R.id.drink,arrayList);
-            listView.setAdapter(arrayAdapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    Intent intent=new Intent(DrinkCategory.this,DetailActivity.class);
-                    intent.putExtra("position",id);
-                    intent.putExtra("product",i);
-                    startActivity(intent);
-                }
-            });
         }catch (SQLiteException e){
             Toast toast=Toast.makeText(this,"Database Unavailable",Toast.LENGTH_SHORT);
             toast.show();
